316f7afdcd8b038386cb4c2edd16c9d7af26a6be
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2015-06-08 15:50:47 -0400

    structure

--- boilerplate.rb
+++ boilerplate.rb
@@ -51,7 +51,61 @@ fs = [
             name: 'gruntfile.js',
             type:'file',
             path: project+'/',
-            contents: ''
+            contents: "
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass:{
+            dev:{
+                options:{
+                    outputStyle:'expanded'
+                },
+                files:{
+                    'css/styles.css' : 'src/scss/application.scss'
+                }
+            }
+        },
+        watch:{
+            js:{
+                files:['src/js/*.js'],
+                tasks:['uglify:dev']
+            },
+            css:{
+                files:['src/scss/*.scss'],
+                tasks:['sass:dev']
+            },
+        },
+        uglify: {
+            options: {
+                sourceMap: true
+            },
+            dev:{
+                options:{
+                    beautify: true,
+                    mangle: false,
+                    compress: false
+                },
+                files: {
+                    'js/app.min.js': ['src/js/vendor/*.js', 'src/js/*.js']
+                }
+            },
+            build: {
+                files: {
+                    'js/app.min.js': ['src/js/vendor/*.js', 'src/js/*.js']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+
+    grunt.registerTask('default',['uglify:build']);
+    grunt.registerTask('dev',['uglify:dev', 'sass:dev']);
+
+};
+"
         },
         {
             name: 'index.html',
@@ -69,10 +123,33 @@ fs = [
         </body>
     </html>
 '
+        },
+        {
+            name:'package.json',
+            type:'file',
+            path: project+'/',
+            contents: '
+{
+  "name": "'+input+'",
+  "version": "0.0.1",
+  "description": "",
+  "main": "index.html",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "grunt": "^0.4.5",
+    "grunt-contrib-uglify": "^0.9.1",
+    "grunt-contrib-watch": "^0.6.1",
+    "grunt-sass": "^1.0.0"
+  }
+}
+'
         }
 
 ]
-
 
 def build_fs_for(f)
     if f[:type] == 'folder'
@@ -85,10 +162,6 @@ def build_fs_for(f)
         File.open(f[:path]+f[:name], 'w'){|file| file.write(f[:contents])}
     end
 end
-
-# system 'mkdir', '-p', project
-# system 'mkdir', '-p', project+'/js'
-
 
 
 fs.each do |f|
